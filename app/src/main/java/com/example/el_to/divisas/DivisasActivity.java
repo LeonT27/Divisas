4b511088cdf30c64a73ba5b1d57dfea90eb606d3
Luis M. Tolentino <luistolentino27@gmail.com>
2018-03-10 12:05:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/el_to/divisas/DivisasActivity.java
@@ -0,0 +1,13 @@
+package com.example.el_to.divisas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DivisasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_divisas);
+    }
+}
